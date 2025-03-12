2da21643747e5ddb9ddab25d0aee32a8097597ba
hardyreinhold <hardykaasik@gmail.com>
2025-03-12 10:51:09 +0200

    moved login from frontend to backend and setup the first endpoint on the frontend

--- client/components/GoogleLogin.tsx
+++ client/components/GoogleLogin.tsx
@@ -1,40 +1,27 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { auth, db } from "../firebase";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const GoogleLogin = () => {
-  const provider = new GoogleAuthProvider();
 
-  const loginWithGoogle = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        console.log(user)
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
+  const backendConnect = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/button-click", {
+        method: "POST",
       });
+
+      const data = await response.json();
+      console.log("Server response:", data);
+    } catch (error) {
+      console.error("Error sending request:", error);
+    }
   };
+
   return (
     <div className="flex justify-center mt-4">
       <button
-        onClick={loginWithGoogle}
+        onClick={backendConnect}
         className="w-full py-2 p-4 font-semibold text-gray-700 bg-white border rounded-md shadow-md hover:bg-gray-100 flex items-center justify-center"
       >
         <Image
