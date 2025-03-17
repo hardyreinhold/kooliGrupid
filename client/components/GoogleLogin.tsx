6e818b46f985a0eb99501836f0460f5f1cd46f52
hardyreinhold <hardykaasik@gmail.com>
2025-03-17 12:39:47 +0200

    added login redirect and saving the user to the database and added the handleGroupcreate  function which sends the group data to the backend

--- client/components/GoogleLogin.tsx
+++ client/components/GoogleLogin.tsx
@@ -1,27 +1,56 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { auth } from "../firebase"
 
 const GoogleLogin = () => {
 
-  const backendConnect = async () => {
-    try {
-      const response = await fetch("http://localhost:4000/button-click", {
-        method: "POST",
-      });
+  const loginWithGoogle = () => {
 
-      const data = await response.json();
-      console.log("Server response:", data);
-    } catch (error) {
-      console.error("Error sending request:", error);
-    }
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user)
+      
+        fetch('http://localhost:4000/googleLogin', {   // connects to backend
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ token, user })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.redirectUrl) {
+            window.location.href = data.redirectUrl; // redirects user to the dashboard of teachers if login was success
+          }
+          console.log('Backend response:', data);
+        })
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.log(error)
+      });
   };
+
 
   return (
     <div className="flex justify-center mt-4">
       <button
-        onClick={backendConnect}
+        onClick={loginWithGoogle}
         className="w-full py-2 p-4 font-semibold text-gray-700 bg-white border rounded-md shadow-md hover:bg-gray-100 flex items-center justify-center"
       >
         <Image
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,58 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { loginWithGoogle } from "./functions/login";
 
 const app = express();
 const PORT = 4000;
-
+const admin = require('firebase-admin');
+const serviceAccount = require('./service/serviceAccountKey.json');
 app.use(cors()); 
 app.use(express.json());
 
 
-app.post("/button-click", (req: Request, res: Response) => {
-    console.log("Button was clicked!");
-    res.json({ message: "Button press received by backend!" });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+
+const db = admin.firestore();
+
+
+// funktsioon salvestab kasutaja database-i
+app.post('/googleLogin', async (req: Request, res: Response) => {
+    try {
+        const { token, user } = req.body as { token: string; user: any };
+
+        console.log("Received token:", token);
+        console.log("Received user:", user);
+
+        //add the logged in user to the database
+        await db.collection("userdata").add({ 
+            username: user.displayName,
+            email: user.email,
+        });
+        res.json({ message: "User authenticated successfully", redirectUrl: "/opetaja/dashboard" }); 
+
+    } catch (error) {
+        console.error("Error adding user data:", error);
+
+        if (!res.headersSent) {
+            res.status(500).json({ error: "Failed to save user data" });
+        }
+    }
 });
+
+
+// post method for recieving group name and code
+app.post('/createGroup', async (req: Request, res: Response) => {
+    try {
+        const { groupName, groupCode } = req.body as {groupName: string, groupCode : string}
+
+        console.log("Received groupname: ", groupName )
+        console.log("Recived groupCode: ", groupCode)
+    } catch (error) {
+        console.error("Error adding group data: ", error);
+    }
+})
 
 
 app.listen(PORT, () => {
